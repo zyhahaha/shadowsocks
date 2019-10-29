5186b3a21914305f1f23124647f959ca005ad4b8
zhaoyang <980355088@qq.com>
2019-10-29 11:49:50 +0800

    update

--- dhtspider/index.js
+++ dhtspider/index.js
@@ -1,3 +1,16 @@
-'use strict'
+// 'use strict'
 
-module.exports = require('./lib/spider')
+// module.exports = require('./lib/spider')
+
+const util = require('util');
+const download = require('./common/download.js');
+
+let _infohash = 'd2474e86c95b19b8bcfdb92bc12c9d44667cfa36';
+_infohash = _infohash.toUpperCase();
+let url = util.format('http://torrage.com/torrent/%s.torrent', _infohash);
+console.log(url)
+
+download(url, (err, data) => {
+    console.log(err)
+    console.log(data);
+})
