716cf5445c9f7180eae27402fd050fdf242867b5
zhaoyang <18355403288@163.com>
2019-10-31 11:18:36 +0800

    update

--- dhtspider/index.js
+++ dhtspider/index.js
@@ -2,15 +2,26 @@
 
 // module.exports = require('./lib/spider')
 
-const util = require('util');
-const download = require('./common/download.js');
+// const util = require('util');
+// const download = require('./common/download.js');
 
-let _infohash = 'd2474e86c95b19b8bcfdb92bc12c9d44667cfa36';
-_infohash = _infohash.toUpperCase();
-let url = util.format('http://torrage.com/torrent/%s.torrent', _infohash);
-console.log(url)
+// let _infohash = 'd2474e86c95b19b8bcfdb92bc12c9d44667cfa36';
+// _infohash = _infohash.toUpperCase();
+// let url = util.format('http://torrage.com/torrent/%s.torrent', _infohash);
+// console.log(url)
 
-download(url, (err, data) => {
-    console.log(err)
-    console.log(data);
-})
+// download(url, (err, data) => {
+//     console.log(err)
+//     console.log(data);
+// })
+
+
+const spider = new (require('./lib/spider'))
+
+// spider.on('ensureHash', (hash, addr)=> console.log(`magnet:?xt=urn:btih:${hash}`))
+
+// spider.on('unensureHash', (hash)=> console.log(hash))
+
+// spider.on('nodes', (nodes)=>console.log('foundNodes'))
+
+spider.listen(6339)
